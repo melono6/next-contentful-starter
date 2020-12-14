c36d2e60da8aee1e4a67238cd30408f531ccc1e0
Jason Emery <jason.emery@rufusleonard.com>
2020-12-14 11:06:19 +0000

    try again

--- pages/index.js
+++ pages/index.js
@@ -79,11 +79,25 @@ export default function Home({ posts }) {
             return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
           })}
         </div>
-        <form netlify name="test-form">
-          <input name="name"></input>
-          <input name="phone"></input>
-          <input name="test"></input>
-          <input type="submit"></input>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </main>
 
